cf9813c0f0e14b3bd9345862c330d233be2c1f2b
philippe mallet <you@example.com>
2024-11-08 23:15:52 +0100

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client';
  const app = express()
  const port = 3000
 
  app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+
+
+  // Configuration de la base de données
+const prisma = new PrismaClient();
+
+async function connect() {
+  try {
+    await prisma.$connect();
+    console.log('Connexion à la base de données réussie !');
+  } catch (error) {
+    console.error('Erreur de connexion à la base de données:', error);
+  }
+}
+
+connect();
   
   app.listen(port, () => {
     console.log(`le serveur demarre bien ${port}`)
